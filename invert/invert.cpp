66743e5a21e7e924efa759f7385b533df9772c20
AniSkyWorker <holyrecycler@mail.ru>
2016-02-16 22:11:26 +0300

    added error messages, and reading matrix from file

--- invert/invert.cpp
+++ invert/invert.cpp
@@ -3,44 +3,61 @@
 #include <boost/numeric/ublas/lu.hpp>
 #include <boost/numeric/ublas/assignment.hpp>
 #include <boost/numeric/ublas/io.hpp>
+#include <fstream>
+#include <iomanip>
 
-
-using namespace boost::numeric::ublas;
-using namespace std;
-
+ 
 template<class T>
-bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse)
+bool InvertMatrix(const  boost::numeric::ublas::matrix<T>& input, boost::numeric::ublas::matrix<T>& inverse)
 {
-	typedef permutation_matrix<std::size_t> pmatrix;
+	typedef  boost::numeric::ublas::permutation_matrix<std::size_t> pmatrix;
 
-	matrix<T> A(input);
+	boost::numeric::ublas::matrix<T> A(input);
 	pmatrix pm(A.size1());
 
-	int res = lu_factorize(A, pm);
+	auto res = boost::numeric::ublas::lu_factorize(A, pm);
 	if (res != 0)
 		return false;
-	inverse.assign(identity_matrix<T>(A.size1()));
-	lu_substitute(A, pm, inverse);
+	inverse.assign(boost::numeric::ublas::identity_matrix<T>(A.size1()));
+	boost::numeric::ublas::lu_substitute(A, pm, inverse);
 
 	return true;
 }
 
 #include "storage_adapters.h"
 
-int main()
+int main(int argc,char * argv[])
 {
-	double initialValues[3][3] = {
-		0, 0, 0,
-		0, 0, 0,
-		0, 0, 0
-	};
+	float inputArray[3][3];
+	std::ifstream inputFile(argv[1]);
 
-	matrix<double> A(3, 3), Z(3, 3);
+	if (inputFile.is_open())
+	{
+		for (std::size_t i = 0; i < 3; i++)
+			for (std::size_t j = 0; j < 3; j++)
+				inputFile >> inputArray[i][j];
+		if (inputFile.fail())
+		{
+			std::cout << "Incorrect matrix in file!";
+			return 1;
+		}
+	}
+	else
+	{
+		std::cout << "Incorrect name of file!";
+		return 1;
+	}
 
-	A = make_matrix_from_pointer(initialValues); 
-	InvertMatrix(A, Z);
+	boost::numeric::ublas::matrix<float> inputMat(3, 3), inverseMat(3, 3);
 
-	cout << "A=" << A << endl << "Z=" << Z << endl;
+	inputMat = boost::numeric::ublas::make_matrix_from_pointer(inputArray);
+	if (InvertMatrix(inputMat, inverseMat))
+		std::cout << std::setprecision(3) << "Inverted matrix =" << inverseMat << std::endl;
+	else
+	{
+		std::cout << "Inputted matrix cannot be inverted";
+		return 1;
+	}
     return 0;
 }
 
